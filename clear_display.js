072f6a9337759e3f44ed463cb64558b987ff5b7b
Zu_rin <s165810t@gmail.com>
2022-02-10 18:43:49 +0900

    setIntervalを使わなくした

--- clear_display.js
+++ clear_display.js
@@ -1,7 +1,25 @@
-setInterval(()=>{
-    let explanation = document.getElementById("kaisetsu");
-    if (explanation.getElementsByTagName("p")[0]){
-        explanation.getElementsByTagName("p")[0].remove();
-        explanation.getElementsByClassName("ansbg")[0].removeAttribute("style");
+const removeBlur = (target) => {
+    if (target.getElementsByTagName("p")[0]){
+        target.getElementsByTagName("p")[0].remove();
+        target.getElementsByClassName("ansbg")[0].removeAttribute("style");
     }
-}, 128);
+}
+
+const target = document.getElementById("kaisetsu");
+const observeOption = {
+    childList: true,
+    subtree: true
+}
+
+const observer = new MutationObserver((mutationList, obs) => {
+    mutationList.forEach((mutation) => {
+        // 要素が追加されれば変更
+        if(mutation.addedNodes.length) {
+            removeBlur(target);
+            // Blur削除1回で監視終了
+            obs.disconnect();
+        }
+    });
+});
+
+observer.observe(target, observeOption);
